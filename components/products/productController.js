76e2e5deff276620422360d16ff77e06f8cef4a5
19127312 <19127312@student.hcmus.edu.vn>
2021-11-29 00:30:30 +0700

    Pagination

--- components/products/productController.js
+++ components/products/productController.js
@@ -1,9 +1,15 @@
 const productService = require("./productService");
 const { ObjectId } = require("mongodb");
 exports.list = async function (req, res) {
-  const products = await productService.listProducts();
-  console.log(products);
-  res.render("product", { products });
+  const products = await productService.listProducts(req.query.page);
+  let totalPage = await productService.totalProductNum();
+  totalPage = Math.ceil(totalPage / 3);
+  console.log(totalPage);
+  res.render("product", {
+    page: req.query.page, // Current Page
+    totalPage, // Total Page
+    products: products,
+  });
 };
 
 exports.item = async function (req, res) {
--- components/products/productService.js
+++ components/products/productService.js
@@ -1,5 +1,17 @@
 const Product = require("./productModel");
+const PAGE_SIZE = 3;
+exports.listProducts = (page) => {
+  console.log(page);
+  if (!page) {
+    page = 0;
+  }
+  if (page < 0) {
+    page = 0;
+  }
+  const Skip = (page - 1) * PAGE_SIZE;
+  page = parseInt(page);
+  return Product.find({}).skip(Skip).limit(PAGE_SIZE);
+};
 
-exports.listProducts = () => Product.find().lean();
-
+exports.totalProductNum = () => Product.countDocuments();
 exports.viewOne = (id) => Product.findOne({ _id: id }).lean();
