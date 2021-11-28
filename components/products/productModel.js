8ff457bb1928b900f1dc2f3559738ecf8361ce26
luong07734 <le.ky.luong@gmail.com>
2021-11-28 15:22:47 +0700

    Add gen in productModel

--- components/products/productModel.js
+++ components/products/productModel.js
@@ -19,6 +19,7 @@ const productSchema = new mongoose.Schema({
   dimensions: String,
   materials: String,
   review: Number,
+  gen: String,
 });
 const product = mongoose.model("Product", productSchema);
 
