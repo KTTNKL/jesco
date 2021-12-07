25eb8063655b6d8af803bb6b1e7432d2557d660d
19127312 <19127312@student.hcmus.edu.vn>
2021-12-07 18:16:38 +0700

    Update Change Password

--- components/auth/authController.js
+++ components/auth/authController.js
@@ -1,5 +1,5 @@
 const userService = require("./userService");
-
+const bcrypt = require("bcrypt");
 exports.register = async (req, res) => {
   const { username, email, password } = req.body;
   const user = await userService.register(username, email, password);
@@ -16,32 +16,26 @@ exports.login = (req, res) => {
   res.render("auth/views/login", { wrongPassword });
 };
 
-exports.updateAccount = async function (req,res) {
+exports.updateAccount = async function (req, res) {
   const user = req.body;
-  try{
-
-  await userService.update(user);
-  if(user['old-password'] !== "" && user['new-password'] !=="")
-  {
-    console.log("if 1");
-    console.log(user['old-password']);
-    console.log(user.current_password);
-    if(bcrypt.compare( user.current_password, user['old-password']))
-    {
-      console.log("if 2");
-      user.password = user['new-password'];
-      await userService.update(user);
-      console.log("password changed successfully");
+  try {
+    await userService.update(user);
+    if (user["old-password"] !== "" && user["new-password"] !== "") {
+      const isValid = await userService.validPassword(
+        user["old-password"],
+        req.user
+      );
+      if (isValid) {
+        user.password = await bcrypt.hash(user["new-password"], 10);
+        await userService.update(user);
+        console.log("password changed successfully");
+      }
     }
+  } catch (err) {}
 
-  }
-
-  }
-  catch(err){}
-
-  res.render("auth/views/account", {user});
+  res.render("auth/views/account", { user });
 };
 
-exports.viewAccount = (req,res)=>{
+exports.viewAccount = (req, res) => {
   res.render("auth/views/account");
 };
--- components/auth/userService.js
+++ components/auth/userService.js
@@ -1,35 +1,39 @@
-const userModel = require('./userModel');
-const bcrypt = require('bcrypt');
+const userModel = require("./userModel");
+const bcrypt = require("bcrypt");
 
-exports.findByUsername = (username) =>{
-    return userModel.findOne({
-        username: username
-    }).lean();
-}
+exports.findByUsername = (username) => {
+  return userModel
+    .findOne({
+      username: username,
+    })
+    .lean();
+};
 
-exports.validPassword =(password, user)=>{
-    return bcrypt.compare(password, user.password);
-}
+exports.validPassword = (password, user) => {
+  return bcrypt.compare(password, user.password);
+};
 
-exports.register= async(username, email, password)=>{
-    const passwordHash = await bcrypt.hash(password, 10);
-    return userModel.create({
-        username: username,
-        email_address: email,
-        password: passwordHash
-    });
-}
+exports.validPasswordForChangePass = (password, passwordInDatabase) => {
+  return bcrypt.compare(password, passwordInDatabase);
+};
+exports.register = async (username, email, password) => {
+  const passwordHash = await bcrypt.hash(password, 10);
+  return userModel.create({
+    username: username,
+    email_address: email,
+    password: passwordHash,
+  });
+};
 
-exports.update = (user)=>{
-    console.log(user);
-    userModel.findOneAndUpdate(
-        {_id: user._id},
-        user,
-        { new: true },
-        (err, doc) => {
-          if (err) {
-            console.log(err);
-          }       
-        }  
-    );
+exports.update = (user) => {
+  userModel.findOneAndUpdate(
+    { _id: user._id },
+    user,
+    { new: true },
+    (err, doc) => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
 };
