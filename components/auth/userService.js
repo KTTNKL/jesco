41f80d7d467771f6980c4527fde3c1d743be8e01
luong07734 <le.ky.luong@gmail.com>
2021-12-19 16:18:25 +0700

    Delete sendgrid. Nodemailer example comment in userService

--- components/auth/userService.js
+++ components/auth/userService.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt");
 const randomstring = require("randomstring");
-const sgMail = require("../../middlewares/sendGrid");
+const nodemailer = require("nodemailer");
 
 const userModel = require("./userModel");
 
@@ -37,25 +37,33 @@ exports.register = async (username, email, password) => {
     activationString: activationString,
   });
   // Send activation string to user mail
-  const link = process.env.DOMAIN_NAME+"/login/activate?email="+email+"&activation-string="+activationString;
+  // const link = process.env.DOMAIN_NAME+"/login/activate?email="+email+"&activation-string="+activationString;
 
-  const msg = {
-    to: email,
-    from: process.env.EMAIL_SENDER,
-    subject: 'JESCO ACCOUNT EMAIL ACTIVATION',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: `<h1>Thanks for register your account with Jesco</h1><p>Please activate your account <a href=${link}>Activate now</a></p>`,
-  };
-  sgMail
-    .send(msg)
-    .then(() => {
-      console.log('Email sent');
-    })
-    .catch((error) => {
-      console.error(error);
-    })
+  // let testAccount = await nodemailer.createTestAccount();
 
+  // // create reusable transporter object using the default SMTP transport
+  // let transporter = nodemailer.createTransport({
+  //   host: "smtp.ethereal.email",
+  //   port: 587,
+  //   secure: false, // true for 465, false for other ports
+  //   auth: {
+  //     user: testAccount.user, // generated ethereal user
+  //     pass: testAccount.pass, // generated ethereal password
+  //   },
+  // });
 
+  // let info = await transporter.sendMail( {
+  //   to: email,
+  //   from: process.env.EMAIL_SENDER,
+  //   subject: 'JESCO ACCOUNT EMAIL ACTIVATION',
+  //   text: 'and easy to do anywhere, even with Node.js',
+  //   html: `<h1>Thanks for register your account with Jesco</h1><p>Please activate your account <a href=${link}>Activate now</a></p>`,
+  // });
+  // console.log("Message sent: %s", info.messageId);
+  // // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // // Preview only available when sending through an Ethereal account
+  // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 };
 
 // exports.activate = async (email, activationString)=>{
